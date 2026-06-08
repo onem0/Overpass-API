b6f81a402dc0ca94dee9880ca904fc896a5adb21
Justin Newberry <justin.newberry@srd.subaru.co.jp>
2026-06-08 10:17:22 -0400

    and here

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -128,7 +128,7 @@ if [[ ! -f /db/init_done ]]; then
 fi
 
 # shellcheck disable=SC2016 # ignore SC2016 (variables within single quotes) as this is exactly what we want to do here
-envsubst '${OVERPASS_MAX_TIMEOUT}' </etc/nginx/nginx.conf.template >/etc/nginx/nginx.conf
+envsubst '${OVERPASS_MAX_TIMEOUT} ${OVERPASS_MAX_SIZE}' </etc/nginx/nginx.conf.template >/etc/nginx/nginx.conf
 
 echo "Starting supervisord process"
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
